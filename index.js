d95016bf010173d24cc866222a1da0a19dba39ba
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-03-27 12:47:31 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+let output=document.getElementById("input");
+function addingEventlistener(){
+    alert("I was clicked")
 }
+output.addEventListener("click",addingEventlistener);
